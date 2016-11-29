f58a2f8c15baf2130b44b5d94029a33a01305596
Jens Piegsa <piegsa@gmail.com>
2016-11-29 17:48:05 +0100

    Initial commit

---
+++ eternal-fortune.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if fortune $@ 2>&1 >/dev/null ; then
+	if [ -z $DELAY ]; then
+		DELAY=10
+	fi
+
+	if [ -z $LOGFILE ]; then
+		while (true)
+		do
+			echo [`date`] `fortune $@`
+			sleep $DELAY
+		done
+	else
+		touch $LOGFILE
+		while (true)
+		do
+			echo `date` `fortune $@` >> $LOGFILE
+			sleep $DELAY
+		done
+	fi
+else
+	echo "Unsupported flag: $@"
+	fortune --help
+fi
